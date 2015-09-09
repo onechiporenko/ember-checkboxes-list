e2ef94021a97ea5cb88e178df0a01b33c06c5424
Oleg Nechiporenko <onechiporenko@apache.org>
2015-09-09 14:21:43 +0300

    Add empty tests file

---
+++ tests/unit/components/checkboxes-list-test.js
@@ -0,0 +1,14 @@
+import {
+  moduleForComponent,
+  test
+} from 'ember-qunit';
+
+
+
+moduleForComponent('checkboxes-list', 'CheckboxesList', {});
+
+test('summary', function (assert) {
+
+  assert.ok(true);
+
+});
